0ea0fd7828e3ebf807a95f5e4e48cd9cb3c89c0b
Danish Lone <cu.16bcs1331@gmail.com>
2020-06-09 23:52:27 +0530

    commit

---
+++ src/main/java/comm/example/controller/HomeController.java
@@ -0,0 +1,12 @@
+package comm.example.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    private String home()
+    {
+        return "index";
+    }
+}
